6bccf2a535200c0eef5154a09b8eb51e973fcca8
Lelouch-1996 <1996jmei16@gmail.com>
2026-09-24 10:39:35 -0600

    2.6. El flujo de la interfaz

--- Assets/colissionManager.cs
+++ Assets/colissionManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class colissionManager : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D coll)
+    public void OnTriggerEnter2D(Collider2D coll)
     {
         Debug.Log("touched something");
     }
